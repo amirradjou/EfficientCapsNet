1f8d7b48116480a9ad5b64c16cc92fdbb70bc708
amirradjou <amir.radical.78@gmail.com>
2023-04-16 06:08:20 -0400

    Optimized the code

--- efficient_capsnet/layers.py
+++ efficient_capsnet/layers.py
@@ -94,30 +94,60 @@ class FeatureMap(tf.keras.layers.Layer):
     def __init__(self, param: CapsNetParam, name: str = "FeatureMap") -> None:
         super(FeatureMap, self).__init__(name=name)
         self.param = param
-
-    def build(self, input_shape: tf.TensorShape) -> None:
-        self.conv1 = tf.keras.layers.Conv2D(input_shape=input_shape[1:],filters=self.param.conv1_filter,kernel_size=self.param.conv1_kernel,strides=self.param.conv1_stride,activation=tf.keras.activations.relu,name="feature_map_conv1")
-        self.norm1 = tf.keras.layers.BatchNormalization(name="feature_map_norm1")
-        self.conv2 = tf.keras.layers.Conv2D(filters=self.param.conv2_filter,kernel_size=self.param.conv2_kernel,strides=self.param.conv2_stride,activation=tf.keras.activations.relu,name="feature_map_conv2")
-        self.norm2 = tf.keras.layers.BatchNormalization(name="feature_map_norm2")
-        self.conv3 = tf.keras.layers.Conv2D(filters=self.param.conv3_filter,kernel_size=self.param.conv3_kernel,strides=self.param.conv3_stride,activation=tf.keras.activations.relu,name="feature_map_conv3")
-        self.norm3 = tf.keras.layers.BatchNormalization(name="feature_map_norm3")
-        self.conv4 = tf.keras.layers.Conv2D(filters=self.param.conv4_filter,kernel_size=self.param.conv4_kernel,strides=self.param.conv4_stride,activation=tf.keras.activations.relu,name="feature_map_conv4")
-        self.norm4 = tf.keras.layers.BatchNormalization(name="feature_map_norm4")
-        self.built = True
+        self.conv1 = tf.keras.layers.Conv2D(
+            filters=self.param.conv1_filter,
+            kernel_size=self.param.conv1_kernel,
+            strides=self.param.conv1_stride,
+            activation=tf.keras.activations.relu,
+            name="feature_map_conv1")
+        self.norm1 = tf.keras.layers.BatchNormalization(
+            name="feature_map_norm1")
+        self.conv2 = tf.keras.layers.Conv2D(
+            filters=self.param.conv2_filter,
+            kernel_size=self.param.conv2_kernel,
+            strides=self.param.conv2_stride,
+            activation=tf.keras.activations.relu,
+            name="feature_map_conv2")
+        self.norm2 = tf.keras.layers.BatchNormalization(
+            name="feature_map_norm2")
+        self.conv3 = tf.keras.layers.Conv2D(
+            filters=self.param.conv3_filter,
+            kernel_size=self.param.conv3_kernel,
+            strides=self.param.conv3_stride,
+            activation=tf.keras.activations.relu,
+            name="feature_map_conv3")
+        self.norm3 = tf.keras.layers.BatchNormalization(
+            name="feature_map_norm3")
+        self.conv4 = tf.keras.layers.Conv2D(
+            filters=self.param.conv4_filter,
+            kernel_size=self.param.conv4_kernel,
+            strides=self.param.conv4_stride,
+            activation=tf.keras.activations.relu,
+            name="feature_map_conv4")
+        self.norm4 = tf.keras.layers.BatchNormalization(
+            name="feature_map_norm4")
 
     def call(self, input_images: tf.Tensor) -> tf.Tensor:
-        feature_maps = self.norm1(self.conv1(input_images))
-        feature_maps = self.norm2(self.conv2(feature_maps))
-        feature_maps = self.norm3(self.conv3(feature_maps))
-        return self.norm4(self.conv4(feature_maps))
+        feature_maps = self.conv1(input_images)
+        feature_maps = self.norm1(feature_maps)
+        feature_maps = self.conv2(feature_maps)
+        feature_maps = self.norm2(feature_maps)
+        feature_maps = self.conv3(feature_maps)
+        feature_maps = self.norm3(feature_maps)
+        feature_maps = self.conv4(feature_maps)
+        return self.norm4(feature_maps)
 
-    def compute_output_shape(self, input_shape: tf.TensorShape) -> tf.TensorShape:
+    def compute_output_shape(self,
+                             input_shape: tf.TensorShape) -> tf.TensorShape:
         output_shape = self.conv1.compute_output_shape(input_shape)
+        output_shape = self.norm1.compute_output_shape(output_shape)
         output_shape = self.conv2.compute_output_shape(output_shape)
+        output_shape = self.norm2.compute_output_shape(output_shape)
         output_shape = self.conv3.compute_output_shape(output_shape)
+        output_shape = self.norm3.compute_output_shape(output_shape)
         output_shape = self.conv4.compute_output_shape(output_shape)
-        return output_shape
+        return self.norm4.compute_output_shape(output_shape)
+
 
 '''PrimaryCap Class is used to extract the primary capsules from the feature maps.
 In building the model, we use the output_shape of the previous layer as the input_shape.
@@ -127,20 +157,30 @@ class PrimaryCap(tf.keras.layers.Layer):
         super(PrimaryCap, self).__init__(name=name)
         self.param = param
 
-    def build(self, input_shape: tf.TensorShape) -> None:
-        self.dconv = tf.keras.layers.Conv2D(input_shape=input_shape[1:],filters=self.param.dconv_filter,kernel_size=self.param.dconv_kernel,strides=self.param.dconv_stride,groups=self.param.dconv_filter,activation=tf.keras.activations.relu,name="primary_cap_dconv")
-        self.reshape = tf.keras.layers.Reshape(target_shape=[-1, self.param.dim_primary_caps],name="primary_cap_reshape")
+        self.dconv = tf.keras.layers.Conv2D(
+            filters=self.param.dconv_filter,
+            kernel_size=self.param.dconv_kernel,
+            strides=self.param.dconv_stride,
+            padding='valid',
+            activation=tf.keras.activations.relu,
+            name="primary_cap_dconv"
+        )
+        self.reshape = tf.keras.layers.Reshape(
+            target_shape=[-1, self.param.dim_primary_caps],
+            name="primary_cap_reshape"
+        )
         self.squash = Squash(name="primary_cap_squash")
-        self.built = True
 
-    def call(self, feature_maps: tf.Tensor) -> tf.Tensor:
-        dconv_outputs = self.dconv(feature_maps)
-        return self.squash(self.reshape(dconv_outputs))
+    def call(self, inputs: tf.Tensor) -> tf.Tensor:
+        dconv_outputs = self.dconv(inputs)
+        reshaped_outputs = self.reshape(dconv_outputs)
+        return self.squash(reshaped_outputs)
 
     def compute_output_shape(self, input_shape: tf.TensorShape) -> tf.TensorShape:
         output_shape = self.dconv.compute_output_shape(input_shape)
         output_shape = self.reshape.compute_output_shape(output_shape)
         return output_shape
+
 
 '''DigitCap Class is used to extract the digit capsules from the primary capsules. 
 For each digit capsule, we use the primary capsules as the input.
